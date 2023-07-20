cc51e16a1f421d0d37b8f2580b026f8595dfa7b3
Tyler Goldsmith <tylergoldsmith2001@gmail.com>
2023-07-20 18:31:19 -0400

    Generate data structures and begin recursive tree generation

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+struct eventNode{
+    struct eventNode ** children;
+    struct eventNode * parent;
+    int taskNum;
+    int timeEnum;
+
+    int arrivalTime;
+    int deadline;
+    int executionTime;
+};
+
+int main(void){
+    printf("In");
+    return 0;
+}
+
+int generateChildren(struct eventNode *parent, int numChildren, int level){
+    int child;
+    struct eventNode* currChild;
+    int childFinish;
+
+    for(child=0 ; child<numChildren ; child++){
+        currChild = malloc(sizeof(struct eventNode));
+
+        childFinish = currChild->executionTime;
+
+        if(parent->timeEnum < currChild->arrivalTime)
+            childFinish += (currChild->arrivalTime);
+        else
+            childFinish += (parent->timeEnum);
+        
+        if(childFinish > currChild->deadline)
+            return 0;
+        else
+            generateChildren(currChild,level,level+1);
+    }
+}
