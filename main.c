1a107dd361bc9e483344cee47a391fd9c26dae22
Tyler Goldsmith <tylergoldsmith2001@gmail.com>
2023-07-23 20:23:17 -0400

    Expanded read in functionality, added debug points

--- main.c
+++ main.c
@@ -36,46 +36,63 @@ int main(void){
     FILE *infp;
     infp = fopen("C:\\Users\\Tyler\\Desktop\\Bratley's_Algorithm_Project\\input.txt","r");
     struct map * inputMap = parse(infp);
+    fclose(infp);
     return 0;
 }
 
 struct map *parse(FILE *infp){
-    printf("In parse\n");
     int numInputCases=0;
     int currNumInputs;
     char currChar;
 
     int readingFile = 0;
     int readingEntry=0;
-    int readingEntryValues=0;
+    int readingValueField=0;
 
+    //Loop to wait for the start of the reading frame in the file
     while (!readingFile){
-        if ((currChar = fgetc(infp))=='[')
+        if ((currChar = fgetc(infp))=='['){
             readingFile=1;
-        printf("%c\n",currChar);
+            printf("Reading Input [%c]\n",currChar);
+        }
     }
-/*
+
     while(readingFile){
         currChar = fgetc(infp);
-        printf("%c ",currChar);
-        if(currChar =='{')
+
+        //Check for the start of individual entries
+        if(currChar =='{'){
+            printf("\tReading entry [%c]\n", currChar);
             readingEntry = 1;
 
-        if(readingEntry && currChar=='"'){
-            if(!readingEntryValues){
-                readingEntryValues = 1;
-                char * tempSTR;
-                int numChar = 0;
-                while(currChar!='"'){
-                    //Read in value
-                }
-                //string case statement to select parameter to be set
-            }
-            else
-                readingEntryValues = 0;
-        }
+            while(readingEntry){
+                currChar = fgetc(infp);
+                //printf("%c ", currChar);
 
-    }*/
+                if(currChar == '\"'){
+                    printf("\tReading Value Field [%c]\n", currChar);
+                    char *currValueField;
+                    int currValueFieldindex = 0;
+
+                    readingValueField = 1;
+
+                    //Iterate through the character names
+                    while((currChar = fgetc(infp)) != '\"'){
+                        printf("\t\tReading Character in Value Field [%c]\n", currChar);
+                        currValueField[currValueFieldindex] = currChar;
+                        currValueFieldindex++;
+                    }
+                    currValueField[currValueFieldindex] = '\0';
+                    printf("Current Value Field: %s\n", currValueField);
+                    readingValueField = 0;
+                if(currChar=='}')
+                    readingEntry=0;
+                }
+            }
+        if(currChar ==']')
+            readingFile = 0;
+        }
+    }
 }
 
 //Recursive function to generate children
