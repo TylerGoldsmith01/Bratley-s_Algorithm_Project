a44b411100ff617c6fc8d2fb477db4e39432adeb
Tyler Goldsmith <tylergoldsmith2001@gmail.com>
2023-07-26 20:49:27 -0400

    Completed Parsing functionality

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <string.h>
 
+#define MAX_FIELD_LENGTH 20
 
 struct map *parse(FILE *);
 
@@ -19,6 +21,7 @@ struct eventNode{
 
 struct mapEntry{
     int key;
+    
     char * name;
     int arrivalTime;
     int deadline;
@@ -47,52 +50,94 @@ struct map *parse(FILE *infp){
 
     int readingFile = 0;
     int readingEntry=0;
-    int readingValueField=0;
+    int readingVariable_Name=0;
+    int readingVariable_Value=0;
+
+    struct map *tempMap;
 
     //Loop to wait for the start of the reading frame in the file
     while (!readingFile){
-        if ((currChar = fgetc(infp))=='['){
+        currChar = fgetc(infp);
+        if (currChar =='['){
             readingFile=1;
             printf("Reading Input [%c]\n",currChar);
         }
     }
 
     while(readingFile){
-        currChar = fgetc(infp);
+        //Wait until you get to an entry to read
+        while (!readingEntry && readingFile){
+            currChar = fgetc(infp);
+            if(currChar =='{'){
+                readingEntry = 1;
+                printf("\tReading entry [%c]\n", currChar);
+            } else if (currChar == ']'){
+                readingFile=0;
+                printf("Done Reading Input File");
+            }
+        }
 
-        //Check for the start of individual entries
-        if(currChar =='{'){
-            printf("\tReading entry [%c]\n", currChar);
-            readingEntry = 1;
-
-            while(readingEntry){
+        while(readingEntry){
+            //Wait until you get to a value field to read
+            while(!readingVariable_Name){
                 currChar = fgetc(infp);
-                //printf("%c ", currChar);
-
                 if(currChar == '\"'){
-                    printf("\tReading Value Field [%c]\n", currChar);
-                    char *currValueField;
-                    int currValueFieldindex = 0;
-
-                    readingValueField = 1;
-
-                    //Iterate through the character names
-                    while((currChar = fgetc(infp)) != '\"'){
-                        printf("\t\tReading Character in Value Field [%c]\n", currChar);
-                        currValueField[currValueFieldindex] = currChar;
-                        currValueFieldindex++;
-                    }
-                    currValueField[currValueFieldindex] = '\0';
-                    printf("Current Value Field: %s\n", currValueField);
-                    readingValueField = 0;
-                if(currChar=='}')
-                    readingEntry=0;
+                    printf("\tReading Variable Name [%c]\n", currChar);
+                    readingVariable_Name = 1;
                 }
             }
-        if(currChar ==']')
-            readingFile = 0;
+                while (readingVariable_Name){
+                    char Variable_Name[MAX_FIELD_LENGTH]; // Assuming MAX_FIELD_LENGTH is a constant defining the maximum length of value fields
+                    int Variable_Name_Index = 0;
+                    currChar = fgetc(infp);
+
+                    while(currChar != '\"'){
+                        printf("\t\tReading Character in Variable Name [%c]\n", currChar);
+                        Variable_Name[Variable_Name_Index] = currChar;
+                        Variable_Name_Index++;
+                        currChar = fgetc(infp);
+                    }
+                    Variable_Name[Variable_Name_Index] = '\0';
+                    printf("Current Value Field: %s\n", Variable_Name);
+                    readingVariable_Name=0;
+                }
+
+                while(!readingVariable_Value){
+                    currChar = fgetc(infp);
+                    if(currChar == ':'){
+                        printf("\tReading Variable Value [%c]\n", currChar);
+                        fgetc(infp);
+                        readingVariable_Value = 1;
+                    }
+                }
+                while (readingVariable_Value){
+                    char Variable_Value[MAX_FIELD_LENGTH]; // Assuming MAX_FIELD_LENGTH is a constant defining the maximum length of value fields
+                    int Variable_Value_Index = 0;
+                    currChar = fgetc(infp);
+                    if(currChar=='\"')
+                        currChar = fgetc(infp);
+                    while(currChar != '\"' && currChar != ',' && currChar != '\n'){
+                        printf("\t\tReading Character in Variable Name [%c]\n", currChar);
+                        Variable_Value[Variable_Value_Index] = currChar;
+                        Variable_Value_Index++;
+                        currChar = fgetc(infp);
+                    }
+                    Variable_Value[Variable_Value_Index] = '\0';
+                    printf("Current Value Field: %s\n", Variable_Value);
+                    //Delay so the current character is always the comma
+                    if(currChar=='\"')
+                        currChar = fgetc(infp);
+                    readingVariable_Value=0;
+                }
+                if(currChar!=','){
+                    while (currChar!='}'){
+                        currChar = fgetc(infp);
+                    }
+                    readingEntry=0;
+                }
         }
     }
+    return tempMap;
 }
 
 //Recursive function to generate children
